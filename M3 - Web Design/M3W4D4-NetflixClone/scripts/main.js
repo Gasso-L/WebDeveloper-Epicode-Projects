f0ec4618d4261186c7b864b511849845e1492f8f
lcero <cerofolini.lorenzo@gmail.com>
2025-03-09 17:13:59 +0100

    Progetto di Fine Modulo 3
    Stato: Completo

--- M3 - Web Design/M3W4D4-NetflixClone/scripts/main.js
+++ M3 - Web Design/M3W4D4-NetflixClone/scripts/main.js
@@ -1,9 +1,20 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const asideBurgerMenu = document.querySelector(".aside-burger-menu");
+const overlay = document.querySelector(".overlay");
 
-burgerMenu.addEventListener("click", ()=>{
-    burgerMenu.classList.toggle("activeBurger");
-    asideBurgerMenu.classList.toggle("show-aside-burger-menu");
+document.addEventListener("click", (e)=>{
+    const isClickInsideAside = asideBurgerMenu.contains(e.target);
+    const isClickInsideBurger = burgerMenu.contains(e.target);
+
+    if (!isClickInsideBurger && !isClickInsideAside){
+      burgerMenu.classList.remove("activeBurger");
+      asideBurgerMenu.classList.remove("show-aside-burger-menu");
+      overlay.classList.remove("active");
+    } else {
+      burgerMenu.classList.toggle("activeBurger");
+      asideBurgerMenu.classList.toggle("show-aside-burger-menu");
+      overlay.classList.toggle("active");
+    }
 })
 
 const swiper = new Swiper('.swiper', {
